dcdb630ac1634cc56f46bd68144d2d4d0f745bf2
jacobduncan00 <jacobduncan00@yahoo.com>
2020-03-20 09:37:11 -0400

    new work

---
+++ Password.py
@@ -0,0 +1,6 @@
+password = input('Enter your password: ')
+if password == 'jacob' or password == 'Jacob':
+    print('That was the correct password')
+else:
+    print('Incorrect password entered!')
+
---
+++ StringRank.py
@@ -0,0 +1,6 @@
+name1 = input('Enter name1: ')
+name2 = input('Enter name2: ')
+if name1 < name2:
+    print(name1,'comes before', name2, 'in the alphabet')
+else:
+    print(name2, 'comes before', name1, 'in the alphabet')
